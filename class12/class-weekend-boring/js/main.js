f64d6c0a302e7e60fe37e4393a3ab19c3ad05dee
Mark Linn <marktlinn@gmail.com>
2022-05-11 10:45:57 +0200

    older projects yet to be pushed

--- class12/class-weekend-boring/js/main.js
+++ class12/class-weekend-boring/js/main.js
@@ -1,10 +1,42 @@
-document.querySelector('#check').addEventListener('click', check)
+document.querySelector('#confirmButton').addEventListener('click', dayCheck);
 
-function check() {
+// this function added for the last par tof the ternary operator to test its ability to make the syntax a little neater.
+  const textReturner = (param) => {
+    return document.querySelector('#demoTextArea').innerHTML = param;
+  }
 
-  const day = document.querySelector('#day').value
+function dayCheck() {
 
-  //Conditionals go here
+    const lessonDay = /(Tuesday)|(Thursday)/i
+    const weekend = /(Saturday)|(Sunday)/i
+    const weekDay = /(Monday)|(Wednesday)|(Friday)/i
+    const day = document.querySelector('#day').value;
 
+  if(day.match(lessonDay)) {
+    return textAddClassDay(), backgroundCodeClassDay(), boxShadowRemoval();
+  } else if (day.match(weekend)){
+    return textAddWeekend(), backgroundCodeWeekend();
+  } day.match(weekDay)? textReturner('Boring day, son!'): textReturner('Ain\'t no day I\'ve ever heard of!')
+}
 
+// If lessonDay returns true the following functions run:
+function textAddClassDay() {
+return textReturner('Class Day!')
+}
+
+function backgroundCodeClassDay() {
+  return document.querySelector('video').style.display = 'block'
+}
+
+function boxShadowRemoval() {
+  document.querySelector('body').style.boxShadow = 'none'
+}
+
+// If day is a weekend the follow functions are invoked:
+function textAddWeekend (){
+  return textReturner('Weekend, yo!')
+}
+
+function backgroundCodeWeekend() {
+  return document.querySelector('body').style.backgroundImage = " URL('./img/weekend.jpg')";
 }
