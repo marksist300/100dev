2dd2bde3813e6a1063aecc09f62b1036872cd0ae
Mark Linn <marktlinn@gmail.com>
2022-05-11 11:46:26 +0200

    updated and simplified the js added more background interactivity

--- class12/class-weekend-boring/js/main.js
+++ class12/class-weekend-boring/js/main.js
@@ -7,36 +7,53 @@ document.querySelector('#confirmButton').addEventListener('click', dayCheck);
 
 function dayCheck() {
 
-    const lessonDay = /(Tuesday)|(Thursday)/i
-    const weekend = /(Saturday)|(Sunday)/i
-    const weekDay = /(Monday)|(Wednesday)|(Friday)/i
+    const lessonDay = /(^Tuesday$)|(^Thursday$)/i;
+    const weekend = /(^Saturday$)|(^Sunday$)/i;
+    const weekDay = /(^Monday$)|(^Wednesday$)|(^Friday$)/i;
     const day = document.querySelector('#day').value;
 
   if(day.match(lessonDay)) {
-    return textAddClassDay(), backgroundCodeClassDay(), boxShadowRemoval();
+    return classDay();
   } else if (day.match(weekend)){
-    return textAddWeekend(), backgroundCodeWeekend();
-  } day.match(weekDay)? textReturner('Boring day, son!'): textReturner('Ain\'t no day I\'ve ever heard of!')
-}
+    return theWeekend();
+  } day.match(weekDay)? boringDay(): noDay();;
+};
 
-// If lessonDay returns true the following functions run:
-function textAddClassDay() {
-return textReturner('Class Day!')
-}
+// If lessonDay returns true the following function runs:
+function classDay() {
+ textReturner('Class Day!');
+ document.querySelector('video').style.display = 'block';
+ document.querySelector('.main-container').style.boxShadow = 'none';
+};
 
-function backgroundCodeClassDay() {
-  return document.querySelector('video').style.display = 'block'
-}
+// If day is a weekend the follow function is invoked:
+function theWeekend(){
+  textReturner('Weekend, yo!');
+  document.querySelector('body').style.backgroundImage = " URL('./img/weekend.jpg')";
+  document.querySelector('body').style.backgroundSize = '50% 65vw';
+  document.querySelector('body').style.backgroundRepeat = 'no-repeat';
+  document.querySelector('body').style.backgroundPositionX = '50%';
+  document.querySelector('.main-container').style.boxShadow = 'none';
+  if(document.querySelector('video').style.display == 'block') {
+    document.querySelector('video').style.display = 'none'
+  };
+};
 
-function boxShadowRemoval() {
-  document.querySelector('body').style.boxShadow = 'none'
-}
-
-// If day is a weekend the follow functions are invoked:
-function textAddWeekend (){
-  return textReturner('Weekend, yo!')
-}
-
-function backgroundCodeWeekend() {
-  return document.querySelector('body').style.backgroundImage = " URL('./img/weekend.jpg')";
-}
+//so called 'Boring day' or the fall back of not a day.
+function boringDay() {
+  textReturner('Boring day, son!');
+  document.querySelector('body').style.backgroundImage = "URL('./img/boring.jpg')";
+  document.querySelector('body').style.backgroundSize = '50% 65vw';
+  document.querySelector('body').style.backgroundRepeat = 'no-repeat';
+  document.querySelector('body').style.backgroundPositionX = '50%';
+  document.querySelector('.main-container').style.boxShadow = 'none';
+  if(document.querySelector('video').style.display == 'block') {
+    document.querySelector('video').style.display = 'none'
+  };
+};
+function noDay() {
+  textReturner('Ain\'t no day I\'ve ever heard of!')
+  if(document.querySelector('video').style.display == 'block') {
+    document.querySelector('video').style.display = 'none'
+  } document.querySelector('body').style.backgroundImage = ''
+};
