5f52432cd7c20b9613f218064789901646347602
Mark Linn <marktlinn@gmail.com>
2022-05-14 16:37:28 +0200

    finished project, simple for repetition, improved the js

--- class12/class-weekend-boring/js/main.js
+++ class12/class-weekend-boring/js/main.js
@@ -1,68 +1,53 @@
-// All written out multiple times, rather than just declaring variables, as I wanted to practice and repeat the syntax.
+//enter a day of the week
+//receive the value
+//check value against regex
+// change the background image according to the day of the week
+//output text based on day of the week
 
-document.querySelector('#confirmButton').addEventListener('click', dayCheck);
 
-// this function added for the last part of the ternary operator to test its ability to make the syntax /// a little neater.
-  const textReturner = (param) => {
-    return document.querySelector('#demoTextArea').innerHTML = param;
-  }
+const confirmButton = document.querySelector('#confirmButton');
+const body = document.querySelector('body');
+const video = document.querySelector('video');
 
-function removal() {
-  return document.querySelector('#confirmButton').remove();
+function textReturner (param) {
+  return document.querySelector('#demoTextArea').innerText = param;
 }
 
-function dayCheck() {
 
-    const lessonDay = /(^Tuesday$)|(^Thursday$)/i;
-    const weekend = /(^Saturday$)|(^Sunday$)/i;
-    const weekDay = /(^Monday$)|(^Wednesday$)|(^Friday$)/i;
-    const day = document.querySelector('#day').value;
+confirmButton.addEventListener('click', dayChecker);
 
-  if(day.match(lessonDay)) {
-    return classDay();
-  } else if (day.match(weekend)){
-    return theWeekend();
-  } day.match(weekDay)? boringDay(): noDay();;
-};
+function dayChecker(){
+  const day = document.querySelector('#day').value;
+  const CLASSDAYS = /(^Tuesday$)|(^Thursday$)/i;
+  const BORINGDAYS = /(^Monday$)|(^Wednesday$)|(^Friday$)/i;
+  const WEEKEND = /(^Saturday$)|(^Sunday$)/i;
+  if(day.match(CLASSDAYS)){
+     return goClass();
+  } else if (day.match(WEEKEND)){
+     return goWeekend();
+  } day.match(BORINGDAYS)? goBoring(): noDay();
+}
 
-// If lessonDay returns true the following function runs:
-function classDay() {
- textReturner('Class Day!');
- document.querySelector('video').style.display = 'block';
- document.querySelector('.main-container').style.boxShadow = 'none';
- document.querySelector('#confirmButton').remove();
-};
 
-// If day is a weekend the follow function is invoked:
-function theWeekend(){
-  textReturner('Weekend, yo!');
-  document.querySelector('body').style.backgroundImage = " URL('./img/weekend.jpg')";
-  document.querySelector('body').style.backgroundSize = '50% 65vw';
-  document.querySelector('body').style.backgroundRepeat = 'no-repeat';
-  document.querySelector('body').style.backgroundPositionX = '50%';
-  document.querySelector('.main-container').style.boxShadow = 'none';
-  document.querySelector('#confirmButton').remove();
-  if(document.querySelector('video').style.display == 'block') {
-    document.querySelector('video').style.display = 'none'
-  };
-};
 
-//so called 'Boring day' or the fall back of not a day.
-function boringDay() {
-  textReturner('Boring day, son!');
-  document.querySelector('body').style.backgroundImage = "URL('./img/boring.jpg')";
-  document.querySelector('body').style.backgroundSize = '50% 65vw';
-  document.querySelector('body').style.backgroundRepeat = 'no-repeat';
-  document.querySelector('body').style.backgroundPositionX = '50%';
-  document.querySelector('.main-container').style.boxShadow = 'none';
-  document.querySelector('#confirmButton').remove();
-  if(document.querySelector('video').style.display == 'block') {
-    document.querySelector('video').style.display = 'none'
-  };
-};
+function goClass(){
+    textReturner('Class day!')
+    video.style.display ='block';
+    confirmButton.remove()
+}
+
+function goBoring() {
+  textReturner('Boring day!')
+  body.classList.toggle('boring')
+  confirmButton.remove()
+}
+
+function goWeekend() {
+  textReturner('Oh, yeah, the Weekend!')
+  body.className = 'weekend'
+  confirmButton.remove()
+}
+
 function noDay() {
-  textReturner('Ain\'t no day I\'ve ever heard of!')
-  if(document.querySelector('video').style.display == 'block') {
-    document.querySelector('video').style.display = 'none'
-  } document.querySelector('body').style.backgroundImage = ''
-};
+  textReturner('Nah, ain\'t no day I done heard of, son')
+}
