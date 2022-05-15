8f9e8cc7bd5023e664b16a90b0bd61644fdcbf6f
Mark Linn <marktlinn@gmail.com>
2022-05-15 18:54:00 +0200

    rewritten for the syntax practice, minimal changes

--- class12/temperature-converter/js/main.js
+++ class12/temperature-converter/js/main.js
@@ -1,16 +1,31 @@
-const formInput = document.querySelector('#temp-input');
-const inputBtn = document.querySelector('#submitBtn');
+const input = document.querySelector('#temp-input');
 const output = document.querySelector('#temp-output');
+const submitBtn = document.querySelector('#submitBtn');
 
-formInput.addEventListener('keypress', (e) => {
-    if(e.key ==='Enter'){
+input.addEventListener('keypress', (e) => {
+    if(e.key === 'Enter'){
         e.preventDefault();
         tempConverter();
-    } else inputBtn.addEventListener('click', tempConverter);
+    } else submitBtn.addEventListener('click', tempConverter)
 });
 
 function tempConverter(){
-    let temp = document.querySelector('#temp-input').value;
+    let temp = input.value;
     temp = (temp * 9/5) + 32;
-    !isNaN(temp)? output.innerText = temp: output.innerText = 'Please enter a Number'
+    !isNaN(temp)? output.innerText = temp: output.innerText = 'Please enter a number'
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
