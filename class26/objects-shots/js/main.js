842d974f27c12063ad4f7ef2b395c91aa4a420e2
Mark Linn <marktlinn@gmail.com>
2022-06-06 14:49:01 +0200

    basic ingredient reset working now, still need to add functionality for carousel buttons.

--- class26/objects-shots/js/main.js
+++ class26/objects-shots/js/main.js
@@ -1,116 +1,3 @@
-// //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
-// class CocktailList {
-//     constructor(input){
-//         this.input = input;
-//         this.index = 0;
-//         this.drinksearchActive = false;
-//         this.drinkSelectionLength = 0;
-//         this.drinkName = '';
-//         this.instructions = '';
-//         this.drinkImage = '';
-
-//     }
-//     reset() {
-//         console.log('reset done')
-//         this.index = 0;
-//         this.drinksearchActive = false;
-//         this.drinkSelectionLength = 0;
-//         this.drinkName = '';
-//         this.instructionsTitle = '';
-//         this.instructions = '';
-//         this.drinkImage = '';
-//         this.fetcher(this.input)
-//     }
-//     prevDrink(){
-//         if(this.index >0){
-//             this.index--
-//             return this.fetcher()
-//         }   else return;
-//     }
-//     nextDrink(){
-//         if(this.index < this.drinkSelectionLength){
-//             this.index++;
-//             return this.fetcher()
-//         } else return;
-//     }
-//     noInput() {
-//         this.input.classList.add('warning')
-//     }
-
-//     thereIsInput(){
-//         if(this.input.classList.contains('warning')) this.input.classList.remove('warning');
-//     }
-
-//     fetcher(){
-//         this.thereIsInput()
-//         const selection = input.value;
-//         fetch(url+selection)
-//         .then(res => res.json())
-//         .then(data => {
-//             console.log(data)
-//             console.log(data.drinks.length)
-//                 this.drinksearchActive = true;
-//                 this.drinkSelectionLength = data.drinks.length;
-//                 this.drinkName = data.drinks[this.index].strDrink;
-//                 this.instructionsTitle = 'Instructions: '
-//                 this.instructions = data.drinks[this.index].strInstructions
-//                 this.drinkImage = data.drinks[this.index].strDrinkThumb;
-//                 this.displayDrink()
-//             })
-//             .catch(err=> {
-//                 `Error: ${err}`
-//             });
-//     }
-
-//     displayDrink(){
-//         nameOutput.innerText = this.drinkName;
-//         instructionsOutput.innerText = this.instructions
-//         imgOutput.src = this.drinkImage;
-//     }
-// }
-
-
-// const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
-// const input = document.querySelector('input');
-
-// // Buttons
-// const submitBtn = document.querySelector('.choice-btn');
-// const buttons = Array.from(document.querySelectorAll('button'));
-// const nameOutput = document.querySelector('#name-output');
-// const instructionsTitle = document.querySelector('h3');
-// const instructionsOutput = document.querySelector('p');
-// const imgOutput = document.querySelector('img');
-// buttons.forEach(btn => btn.addEventListener('click', e => 
-//     {
-//         if(e.target.classList.contains('select-btn')) {
-//             const btnId = e.target.id;
-//             if (input.value == '') return cocktailList.noInput();
-//             else if(btnId === 'submit-btn'){
-//                 cocktailList.fetcher(input.value);
-//             } else if(btnId === 'submit-btn' && cocktailList.drinkSearchActive === true){
-//                 cocktailList.reset()
-//                 cocktailList.fetcher(input.value);
-//             } else if(cocktailList.drinksearchActive === true && btnId === 'prev-btn'){
-//                 return cocktailList.prevDrink();
-//             } else if(cocktailList.drinksearchActive === true && btnId === 'next-btn'){
-//                 return cocktailList.nextDrink();
-//             }
-//         } 
-//     }
-// ))
-
-
-
-// const cocktailList = new CocktailList(input)
-/*
-To take a user input and stor the value.
-To set a function to retreive data.
-take that data, parsed as JSON and search for the user input.value
-To then display a picture of the user's choice of cocktail, as well as its title, instruction for how to make it.
-Retrieve all results as an array of 'n' elements. store all those elements as images in a carousel.
-On clicking to the next image update all the other info (instructions, name etc to represent the image).
-*/
-
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 //input received from the user
 const input = document.querySelector('input');
@@ -124,6 +11,7 @@ const imgOutput = document.querySelector('img');
 const carouselBtns = document.querySelector('.carousel');
 const noInputFound = document.querySelector('.noInput');
 const ingredientSection = document.querySelector('.ingredients-section')
+
 // variables that store the api data + index values for DOM rendering
 let drinkData;
 let selectionActive = false;
@@ -222,7 +110,6 @@ function retriever(selection){
                     drinkImages.push(elem.strDrinkThumb)
                     drinkInstructions.push(elem.strInstructions)
                 });
-                ingredientsRetriever(index);
             }
             displayDrinkData(drinkNames, drinkImages, drinkInstructions, index);
         })
@@ -231,7 +118,20 @@ function retriever(selection){
         })
 }
 
+function ingredientReset(index){
+    ingrdientsList = [];
+    while (ingredientSection.hasChildNodes()) {
+        ingredientSection.removeChild(ingredientSection.firstChild);
+    }
+    console.log(ingredientSection.firstChild)
+
+    ingredientsRetriever(index);
+}
+
 function ingredientsRetriever(index){
+    if(ingredientSection.children.length >0){
+        ingredientReset(index)
+    } else{
     let value = 1;
     let result;
     while(result !== null){
@@ -239,6 +139,17 @@ function ingredientsRetriever(index){
         if(result !== null) ingrdientsList.push(result)
         value++;
     }
+    ingredientsDisplay();
+  }
+}
+
+function ingredientsDisplay(){
+    ingrdientsList.forEach(ingredient=> {
+        let substance = document.createElement('li')
+        let item = document.createTextNode(ingredient)
+        substance.appendChild(item)
+        ingredientSection.appendChild(substance)
+    })
 }
 
 function displayDrinkData(drinkName, drinkImage, drinkInstructions, index=0) {
@@ -252,10 +163,5 @@ function displayDrinkData(drinkName, drinkImage, drinkInstructions, index=0) {
     carouselBtns.style.visibility = 'visible';
     instructionsTitle.innerText = 'Instructions: ';
     instructionsOutput.innerText = drinkInstructions[i];
-    ingrdientsList.map(ingredient=> {
-        let substance = document.createElement('li')
-        let item = document.createTextNode(ingredient)
-        substance.appendChild(item)
-        ingredientSection.appendChild(substance)
-    })
+    ingredientsRetriever(index)
 }
