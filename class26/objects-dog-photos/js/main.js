baf5c44be4493726209d1395d3a45d433903edac
Mark Linn <marktlinn@gmail.com>
2022-06-07 16:00:57 +0200

    started working on dog aPI

--- class26/objects-dog-photos/js/main.js
+++ class26/objects-dog-photos/js/main.js
@@ -1,15 +1,13 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
 const output = document.querySelector('img');
 
+// 'https://dog.ceo/api/breeds/image/random'
 
 fetch('https://dog.ceo/api/breeds/image/random')
-    .then(res => res.json())
-    .then(data =>{
+    .then(res=> res.json())
+    .then(data=>{
         console.log(data.message)
-        output.src = data.message;
     })
-    .catch(err => {
-    console.log(`Error: ${err}`)
-    });
-
-
+    .catch(err=> {
+        console.log(`Error: ${err}`)
+    })
