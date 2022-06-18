f1d8d55fa355a662fa5ac49da2d883b33f539410
Mark Linn <marktlinn@gmail.com>
2022-06-18 21:42:13 +0200

    add responsiveness to object: when player is toggled, the turn is logged in the object with the Boolean true and the other playersTurn setting is set to false

--- ticTacToe/js/main.js
+++ ticTacToe/js/main.js
@@ -10,7 +10,8 @@ class PlayerSelection{
 
 const player1Naughts = new PlayerSelection('O', true);
 const player2Crosses = new PlayerSelection('X', false);
-
+console.log(player1Naughts.playersTurn)
+console.log(player2Crosses.playersTurn)
 const playerToggle = Array.from(document.querySelectorAll('.toggleSwitch'))
 const squares = Array.from(document.querySelectorAll('.square'));
 
@@ -18,9 +19,18 @@ const squares = Array.from(document.querySelectorAll('.square'));
 playerToggle.forEach(item=> item.addEventListener('change', e=>{
     if(e.target.checked === true && e.target.id === 'p1Toggle'){
         document.querySelector('#p2Toggle').checked = false
+        player1Naughts.playersTurn = true;
+        player2Crosses.playersTurn = false;
+        console.log('p1:',player1Naughts.playersTurn)
+        console.log('p2:',player2Crosses.playersTurn)
+
         console.log(e.target.checked, e.target.id)
     } else if (e.target.checked === true && e.target.id === 'p2Toggle'){
         document.querySelector('#p1Toggle').checked = false;
+        player1Naughts.playersTurn = false;
+        player2Crosses.playersTurn = true;
+        console.log('p1:',player1Naughts.playersTurn)
+        console.log('p2:',player2Crosses.playersTurn)
         console.log(e.target.checked, e.target.id)
     }
 }))
