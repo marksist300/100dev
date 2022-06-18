58b682aeabc87a14760d5ef09788b10b7b733b24
Mark Linn <marktlinn@gmail.com>
2022-06-18 22:29:40 +0200

    add toggle user functionality, automated on click to switch user, still needs tweaking

--- ticTacToe/js/main.js
+++ ticTacToe/js/main.js
@@ -13,25 +13,48 @@ const player2Crosses = new PlayerSelection('X', false);
 const playerToggle = Array.from(document.querySelectorAll('.toggleSwitch'))
 const squares = Array.from(document.querySelectorAll('.square'));
 
-
-playerToggle.forEach(item=> item.addEventListener('change', e=>{
-    if(e.target.checked === true && e.target.id === 'p1Toggle'){
-        document.querySelector('#p2Toggle').checked = false
-        player1Naughts.playersTurn = true;
-        player2Crosses.playersTurn = false;
-    } else if (e.target.checked === true && e.target.id === 'p2Toggle'){
+function switchPlayer(){
+    if(player1Naughts.playersTurn === true){
+        document.querySelector('#p2Toggle').checked = true
         document.querySelector('#p1Toggle').checked = false;
         player1Naughts.playersTurn = false;
         player2Crosses.playersTurn = true;
+    } else if (player2Crosses.playersTurn === true){
+        document.querySelector('#p1Toggle').checked = true;
+        document.querySelector('#p2Toggle').checked = false
+        player1Naughts.playersTurn = true;
+        player2Crosses.playersTurn = false;
+    }   
+}
+
+playerToggle.forEach(item=> item.addEventListener('change', e=>{
+    if(e.target.checked === true && e.target.id === 'p1Toggle'){
+        switchPlayer()
+        console.log('p1',player1Naughts.playersTurn)
+        console.log('p2',player2Crosses.playersTurn)
+    } else if (e.target.checked === true && e.target.id === 'p2Toggle'){
+        switchPlayer()
+        console.log('p1',player1Naughts.playersTurn)
+        console.log('p2',player2Crosses.playersTurn)
     }
 }))
 
 squares.forEach(elem=> elem.addEventListener('click', e=>{
     console.log(e.target.id)
     playerToggle.forEach(player=> {
-        if(player.checked === true && player.id === 'p1Toggle'){
-            e.target.innerText = 'O'
-        } else if(player.checked === true && player.id === 'p2Toggle')
-            e.target.innerText = 'X'
+        if(player1Naughts.playersTurn === true){
+            console.log(e.target)
+            e.target.innerText = player1Naughts.type;
+            console.log(player1Naughts.type)
+            console.log('p1',player1Naughts.playersTurn)
+            console.log('p2',player2Crosses.playersTurn)
+        } else if(player2Crosses.playersTurn === true){
+            console.log(e.target)
+            e.target.innerText = player2Crosses.type;
+            console.log('p1',player1Naughts.playersTurn)
+            console.log('p2',player2Crosses.playersTurn)
+        }
     })
+    switchPlayer()
+
 }))
