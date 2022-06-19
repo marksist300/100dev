781d59b2b24e6ae1a49116957a3c688f32a7180e
Mark Linn <marktlinn@gmail.com>
2022-06-19 15:58:08 +0200

    Commit for class finished last week

--- class27/local-storage-botScore-button/js/main.js
+++ class27/local-storage-botScore-button/js/main.js
@@ -1,3 +1,17 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
+if(!localStorage.getItem('botScore')) {
+    localStorage.setItem('botScore', 0)
+}
 
+const increment = document.querySelector('#increment');
+const outputNum = document.querySelector('#total');
+outputNum.innerText = localStorage.getItem('botScore');
+increment.addEventListener('click', addOne);
+
+function addOne(){
+    let totalBotScore = localStorage.getItem('botScore');
+    totalBotScore = Number(totalBotScore) + Number(1);
+    localStorage.setItem('botScore', totalBotScore);
+    outputNum.innerText = localStorage.getItem('botScore');
+}
 
