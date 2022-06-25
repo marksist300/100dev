4e8dbce5077f652c7980ec3f7e8ec2c88beaa18d
Mark Linn <marktlinn@gmail.com>
2022-06-25 21:44:34 +0200

    API async/await practice done, connecting to dogCEO API with async/await syntax

--- class36/api-template/js/main.js
+++ class36/api-template/js/main.js
@@ -1,1 +1,12 @@
 //Make an api request using async await
+
+
+async function doggoAPI(){
+    const url = 'https://dog.ceo/api/breeds/image/random';
+    const response = await fetch(url);
+    const data = await response.json();
+    const photo = await data.message;
+    document.querySelector('img').src = photo;
+}
+
+document.querySelector('button').addEventListener('click', doggoAPI);
