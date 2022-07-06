92cf7cfcffe4d933f560c1fe6b2532e69641ceb9
Mark Linn <marktlinn@gmail.com>
2022-07-06 18:56:51 +0200

    Basic of server setup to get API live

---
+++ class39/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const figlet = require("figlet");
+const HERO_DATA = require("./Hero_API.json")
+const PORT = 8000;
+
+app.get("/", (req,res)=>{
+    res.sendFile(`${__dirname}/index.html`);
+})
+
+app.get("/api/:name", (req,res) => {
+    let superHeroName = req.params.name[0].toUpperCase() + req.params.name.slice(1).toLowerCase();
+    console.log(superHero);
+    let heroInJson = JSON.stringify(HERO_DATA[superHero]);
+    if(HERO_DATA[superHero]){
+        res.send(heroInJson)
+    }
+    else{
+        figlet("404 : Hero Not Found!", (err,data)=>{
+            if(err){
+                console.log(`Error: ${err}`);
+                return;
+            }
+            res.write(data);
+            res.end;
+        })
+    }
+
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on Port: ${PORT}`);
+})
