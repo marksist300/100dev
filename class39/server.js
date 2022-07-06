bfbb5a302a631c39a0735df0e8c611534a51e197
Mark Linn <marktlinn@gmail.com>
2022-07-06 18:14:02 +0200

    basic API working with simple 404 page handler

--- class39/server.js
+++ class39/server.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const app = express();
+const superHeroes = require("./api.json")
 const PORT = 8000;
+const figlet = require("figlet")
+
+console.log(__dirname + "/index.html")
 
 app.get("/", (req,res)=>{
-    res.sendFile(`${__dirname}index.html`)
+    res.sendFile(`${__dirname}/index.html`)
 })
 
-app.get("/api", (req,res)=>{
-    res.sendFile(`${__dirname}api.json`)
+app.get("/api/:name", (req,res)=>{
+    let heroName = req.params.name[0].toUpperCase() + req.params.name.slice(1).toLowerCase();
+    console.log(req.params.name)
+    if(superHeroes[heroName]){
+        let heroInJson = JSON.stringify(superHeroes[heroName])
+        res.send(heroInJson)
+    }
+    else {
+        figlet('404! Hero Not Found', function(err, data) {
+            if (err) {
+                console.log('Not found');
+                console.dir(err);
+                return;
+            }
+            res.write(data);
+            res.end();
+        });
+    }
 })
 
 app.listen(PORT, ()=>{
